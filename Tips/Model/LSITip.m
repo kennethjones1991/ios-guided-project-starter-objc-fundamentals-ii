2f125b2f21c9e22fdd69fcb52fa09fc1e323e052
Kenneth Jones <kennethjones1991@outlook.com>
2020-11-16 14:58:09 -0700

    Added model file to project

---
+++ Tips/Model/LSITip.m
@@ -0,0 +1,13 @@
+//
+//  LSITip.m
+//  Tips
+//
+//  Created by Kenneth Jones on 11/16/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "LSITip.h"
+
+@implementation LSITip
+
+@end
